90d119d0d21ad7683d24087e7470200620d5577c
Jisu Sim <rlj1202@gmail.com>
2019-12-03 12:12:56 +0900

    Deleted manager login button in navigation bar.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,13 @@
 <template>
-  <div class="min-h-screen flex flex-col">
+  <div class="max-h-screen flex flex-col">
     <div class="p-4 shadow flex flex-row justify-between items-center">
       <div class="font-bold text-lg">
-        Self Rending System
+        <nuxt-link to="/">
+          Self Rending System
+        </nuxt-link>
       </div>
-      <nuxt-link to="/login" class="text-sm">
-        관리자 로그인
-      </nuxt-link>
     </div>
-    <nuxt class="flex-grow" />
+    <nuxt />
   </div>
 </template>
 
