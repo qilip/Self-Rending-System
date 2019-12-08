8d339cf067684e8810693a5c0598890463d9224f
qilip <qilip@qilip.io>
2019-12-08 18:59:53 +0900

    Modify managing page to be responsive

--- pages/managing.vue
+++ pages/managing.vue
@@ -14,7 +14,7 @@
         >
           <h2 class="px-4 py-2">
             <button v-on:click="currentTab = 0" class="font-semibold">
-              품목 관리
+              물품 관리
             </button>
           </h2>
         </li>
@@ -43,101 +43,107 @@
 
     <div>
       <div v-show="currentTab == 0" class="p-6">
-        <h1 class="text-5xl font-bold mb-6">
-          품목 관리
-        </h1>
+        <div class="flex flex-wrap m-4">
+          <div class="p-4 w-full md:max-w-xl">
+            <h1 class="text-3xl font-bold mb-6">
+              품목 관리
+            </h1>
 
-        <menu-block class="my-6 max-w-xl">
-          <menu-item-heading>
-            목록
-          </menu-item-heading>
-          <template v-for="desc in itemDescriptions">
-            <hr>
-            <menu-item-spacer />
-            <hr>
-            <menu-item-value v-bind:value="desc.name" name="이름" />
-            <hr>
-            <menu-item-value v-bind:value="desc.id" name="품번" />
-            <hr>
-            <menu-item-value v-bind:value="desc.price + ' 원'" name="가격" />
-            <hr>
-            <menu-item-value v-bind:value="desc.items.join(', ')" name="물품 목록" />
-          </template>
-        </menu-block>
+            <menu-block class="my-6 max-w-xl">
+              <menu-item-heading>
+                품목 목록
+              </menu-item-heading>
+              <template v-for="desc in itemDescriptions">
+                <hr>
+                <menu-item-spacer />
+                <hr>
+                <menu-item-value v-bind:value="desc.name" name="이름" />
+                <hr>
+                <menu-item-value v-bind:value="desc.id" name="품목 번호" />
+                <hr>
+                <menu-item-value v-bind:value="desc.price + ' 원'" name="대여 보증금" />
+                <hr>
+                <menu-item-value v-bind:value="desc.items.join(', ')" name="물품 시리얼" />
+              </template>
+            </menu-block>
 
-        <menu-block class="my-6 max-w-xl">
-          <menu-item-heading>
-            추가
-          </menu-item-heading>
-          <hr>
-          <menu-item-spacer />
-          <hr>
-          <menu-item-input v-model="name" name="이름" placeholder="이름" />
-          <hr>
-          <menu-item-input v-model="price" name="가격" placeholder="가격" />
-          <hr>
-          <menu-item-button v-on:click.native="addItemDescription()" name="추가" />
-        </menu-block>
+            <menu-block class="my-6 max-w-xl">
+              <menu-item-heading>
+                추가
+              </menu-item-heading>
+              <hr>
+              <menu-item-spacer />
+              <hr>
+              <menu-item-input v-model="name" name="이름" placeholder="이름" />
+              <hr>
+              <menu-item-input v-model="price" name="가격" placeholder="대여 보증금" />
+              <hr>
+              <menu-item-button v-on:click.native="addItemDescription()" name="추가" />
+            </menu-block>
 
-        <menu-block class="my-6 max-w-xl">
-          <menu-item-heading>
-            삭제
-          </menu-item-heading>
-          <hr>
-          <menu-item-spacer />
-          <hr>
-          <menu-item-input v-model="itemID" name="품번" placeholder="id" />
-          <hr>
-          <menu-item-button v-on:click.native="deleteItemDescription()" name="삭제" />
-        </menu-block>
+            <menu-block class="my-6 max-w-xl">
+              <menu-item-heading>
+                삭제
+              </menu-item-heading>
+              <hr>
+              <menu-item-spacer />
+              <hr>
+              <menu-item-input v-model="itemID" name="품목 번호" placeholder="id" />
+              <hr>
+              <menu-item-button v-on:click.native="deleteItemDescription()" name="삭제" />
+            </menu-block>
+          </div>
 
-        <h1 class="text-5xl font-bold my-6">
-          물품 관리
-        </h1>
+          <div class="p-4 w-full md:max-w-xl">
+            <h1 class="text-3xl font-bold mb-6">
+              물품 관리
+            </h1>
 
-        <menu-block class="my-6 max-w-xl">
-          <menu-item-heading>
-            물품 목록
-          </menu-item-heading>
-          <template v-for="item in items">
-            <hr>
-            <MenuItemSpacer />
-            <hr>
-            <MenuItemValue v-bind:value="item.serialNumber" name="시리얼 번호" />
-            <hr>
-            <MenuItemValue v-bind:value="item.status" name="상태" />
-            <hr>
-            <MenuItemValue v-bind:value="item.itemId" name="종류" />
-          </template>
-        </menu-block>
+            <menu-block class="my-6 max-w-xl">
+              <menu-item-heading>
+                물품 목록
+              </menu-item-heading>
+              <template v-for="item in items">
+                <hr>
+                <MenuItemSpacer />
+                <hr>
+                <MenuItemValue v-bind:value="item.serialNumber" name="시리얼 번호" />
+                <hr>
+                <MenuItemValue v-bind:value="item.status" name="상태" />
+                <hr>
+                <MenuItemValue v-bind:value="item.itemId" name="품목 번호" />
+              </template>
+            </menu-block>
 
-        <menu-block class="my-6 max-w-xl">
-          <menu-item-heading>
-            물품 추가
-          </menu-item-heading>
-          <hr>
-          <MenuItemSpacer />
-          <hr>
-          <MenuItemInput v-model="itemID" name="품번" placeholder="id" />
-          <hr>
-          <MenuItemButton v-on:click.native="addItem()" name="추가" />
-        </menu-block>
+            <menu-block class="my-6 max-w-xl">
+              <menu-item-heading>
+                물품 추가
+              </menu-item-heading>
+              <hr>
+              <MenuItemSpacer />
+              <hr>
+              <MenuItemInput v-model="itemID" name="품목 번호" placeholder="id" />
+              <hr>
+              <MenuItemButton v-on:click.native="addItem()" name="추가" />
+            </menu-block>
 
-        <menu-block class="my-6 max-w-xl">
-          <menu-item-heading>
-            물품 삭제
-          </menu-item-heading>
-          <hr>
-          <MenuItemSpacer />
-          <hr>
-          <MenuItemInput v-model="serialNumber" name="시리얼 번호" placeholder="id" />
-          <hr>
-          <MenuItemButton v-on:click.native="deleteItem()" name="삭제" />
-        </menu-block>
+            <menu-block class="my-6 max-w-xl">
+              <menu-item-heading>
+                물품 삭제
+              </menu-item-heading>
+              <hr>
+              <MenuItemSpacer />
+              <hr>
+              <MenuItemInput v-model="serialNumber" name="시리얼 번호" placeholder="id" />
+              <hr>
+              <MenuItemButton v-on:click.native="deleteItem()" name="삭제" />
+            </menu-block>
+          </div>
+        </div>
       </div>
 
       <div v-show="currentTab == 1" class="p-6">
-        <h1 class="text-5xl font-bold mb-6">
+        <h1 class="text-3xl font-bold mb-6">
           사용자 관리
         </h1>
 
@@ -149,7 +155,7 @@
             <hr>
             <MenuItemSpacer />
             <hr>
-            <MenuItemValue v-bind:value="customer.studentId" name="아이디" />
+            <MenuItemValue v-bind:value="customer.studentId" name="학번" />
             <hr>
             <MenuItemValue v-bind:value="customer.credit" name="보증금" />
           </template>
@@ -197,7 +203,7 @@
       </div>
 
       <div v-show="currentTab == 2" class="p-6">
-        <h1 class="text-5xl font-bold mb-6">
+        <h1 class="text-3xl font-bold mb-6">
           정책 관리
         </h1>
       </div>
